7749817bf0d783f7be0a6a8963069c524e782630
Mariyan Slesar <marianslesar.08@gmail.com>
2026-01-25 19:07:05 +0200

    chore: add architecture tests for Execution assembly

---
+++ tests/RevitEditorPlayground.ArchitectureTests/Execution/ExecutionModulesTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using NetArchTest.Rules;
+using RevitEditorPlayground.Execution.InMemory;
+using RevitEditorPlayground.Execution.InProcess;
+using RevitEditorPlayground.Shared;
+using Shouldly;
+
+namespace RevitEditorPlayground.ArchitectureTests.Execution;
+
+public class ExecutionModulesTests
+{
+    private static Assembly ExecutionAssembly => typeof(InMemoryExecution).Assembly; 
+    [Fact]
+    public void InMemory_ShouldNotDependOnInProcess()
+    {
+        var inMemoryNamespace = typeof(InMemoryExecution).Namespace.NotNull();
+        var inProcessNamespace =  typeof(InProcessExecution).Namespace.NotNull();
+
+        Types.InAssembly(
+                assembly: ExecutionAssembly
+            )
+            .That()
+            .ResideInNamespace(inMemoryNamespace)
+            .ShouldNot()
+            .HaveDependencyOn(
+                dependency: inProcessNamespace
+            )
+            .GetResult()
+            .IsSuccessful
+            .ShouldBeTrue();
+
+    }
+    
+    [Fact]
+    public void InProcess_ShouldNotDependOnInMemory()
+    {
+        var inMemoryNamespace = typeof(InMemoryExecution).Namespace.NotNull();
+        var inProcessNamespace =  typeof(InProcessExecution).Namespace.NotNull();
+
+        Types.InAssembly(
+                assembly: ExecutionAssembly
+            )
+            .That()
+            .ResideInNamespace(inProcessNamespace)
+            .ShouldNot()
+            .HaveDependencyOn(
+                dependency: inMemoryNamespace
+            )
+            .GetResult()
+            .IsSuccessful
+            .ShouldBeTrue();
+
+    }
+}
